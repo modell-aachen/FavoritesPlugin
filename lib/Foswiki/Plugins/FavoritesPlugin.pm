c4846e5fb01a0968ed6548c560ab78497c1476b5
Jan Krüger <krueger@modell-aachen.de>
2013-04-19 16:25:44 +0200

    Release 0.9.2

--- lib/Foswiki/Plugins/FavoritesPlugin.pm
+++ lib/Foswiki/Plugins/FavoritesPlugin.pm
@@ -11,8 +11,8 @@ use Foswiki::Func ();
 use Foswiki::Meta ();
 use Foswiki::Plugins ();
 
-our $VERSION = '0.9.1';
-our $RELEASE = '0.9.1';
+our $VERSION = '0.9.2';
+our $RELEASE = '0.9.2';
 our $SHORTDESCRIPTION = 'Allow users to bookmark topics/attachments within the wiki';
 our $NO_PREFS_IN_TOPIC = 1;
 
